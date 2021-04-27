e66470676f3aa716b38b6d5ac6b808390cba96ba
chengyao <bigyao@139.com>
2021-04-27 21:40:33 +0800

    门面支持

--- src/View/ViewService.php
+++ src/View/ViewService.php
@@ -38,3 +38,22 @@ namespace Max\View {
         }
     }
 }
+
+namespace Max\Facade {
+
+    /**
+     * @method static render(string $template, $params = [])
+     * Class View
+     * @package Max\Facade
+     */
+    class View extends Facade
+    {
+
+        protected static function getFacadeClass()
+        {
+            return 'view';
+        }
+
+    }
+
+}
